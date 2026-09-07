4aa6b553f88125b99dab990ae4786b3a1cfdace6
laveeza <lveezajamshed@gmail.com>
2026-09-07 16:34:41 +0500

    feat: enable public access for posts and comments and fix nested schema calls in migration

--- database/migrations/2026_08_24_150405_add_user_id_to_posts_table.php
+++ database/migrations/2026_08_24_150405_add_user_id_to_posts_table.php
@@ -12,9 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            Schema::table('posts', function (Blueprint $table) {
-                $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            });
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
         });
     }
 
@@ -24,10 +22,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            Schema::table('posts', function (Blueprint $table) {
-                $table->dropForeign(['user_id']);
-                $table->dropColumn('user_id');
-            });
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
         });
     }
 };
