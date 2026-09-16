b31658e539f6c2a63951de429d9fe96269ff4b49
laveeza <lveezajamshed@gmail.com>
2026-09-16 19:42:55 +0500

    feat: implement real-time user follow notifications with Laravel Reverb

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use App\Notifications\UserFollowed;
+use App\Models\Follow;
 
 class FollowController extends Controller
 {
@@ -16,13 +18,17 @@ class FollowController extends Controller
             return response()->json(['message' => 'You cannot follow yourself.'], 422);
         }
 
-        $follow = $authUser->following()->where('followed_id', $user->id)->first();
+        // $follow = $authUser->following()->where('followed_id', $user->id)->first();
+        $follow = Follow::where('follower_id', $authUser->id)
+            ->where('followed_id', $user->id)
+            ->first();
 
         if ($follow) {
             $follow->delete();
             $followed = false;
         } else {
             $authUser->following()->attach($user->id);
+            $user->notify(new UserFollowed($authUser));
             $followed = true;
         }
 
---
+++ app/Notifications/UserFollowed.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use App\Models\User;
+use Illuminate\Notifications\Messages\BroadcastMessage;
+
+class UserFollowed extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(public User $follower) {}
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database', 'broadcast'];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'follower_id' => $this->follower->id,
+            'name' => $this->follower->name,
+            'avatar'      => $this->follower->avatar_url ?? null,
+            'message'     => "{$this->follower->name} started following you.",
+        ];
+    }
+
+    public function toBroadcast(object $notifiable): BroadcastMessage
+    {
+        return new BroadcastMessage([
+            'follower_id' => $this->follower->id,
+            'name' => $this->follower->name,
+            'avatar'      => $this->follower->avatar_url ?? null,
+            'message'     => "{$this->follower->name} started following you.",
+        ]);
+    }
+}
